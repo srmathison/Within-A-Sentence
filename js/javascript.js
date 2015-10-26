104e43c6d9c11559f57dc797b9aaff7e70055f2a
Stephanie <stephanierenee.mathison@gmail.com>
2015-10-26 11:17:23 -0400

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+// console.log("hello")
+$(document).ready(function(){
+
+	
+	// var newQuotes = _.shuffle(quotes);
+	// console.log(newQuotes);
+	
+	
+	// for (i = 0; i < quotes.length; i++){
+	// 	console.log(_.shuffle(quotes));
+	// }
+
+
+
+
+// empty safety array :)
+var myArray = [];
+
+// array with strings
+var quotes = [
+		"here is a quote",
+		"this is another",
+		"why not add one more"
+	];
+// randomly select string from array
+var randomIndex = Math.floor(Math.random() * quotes.length); 
+var randomString = quotes[randomIndex];
+// select word from selected string
+
+// shuffle word
+// display string
+//
+
+});
