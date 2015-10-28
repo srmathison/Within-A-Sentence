fc85c647c30bac04cdaefef2f215a3fd6190843b
Stephanie <stephanierenee.mathison@gmail.com>
2015-10-28 14:33:10 -0400

    suffle new word, join new string

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,32 @@
-// console.log("hello")
 $(document).ready(function(){
 
-	
-	// var newQuotes = _.shuffle(quotes);
-	// console.log(newQuotes);
-	
-	
-	// for (i = 0; i < quotes.length; i++){
-	// 	console.log(_.shuffle(quotes));
-	// }
-
-
-
-
-// empty safety array :)
-var myArray = [];
-
-// array with strings
+// array with strings of quotes
 var quotes = [
 		"here is a quote",
 		"this is another",
 		"why not add one more"
 	];
-// randomly select string from array
+// randomly select string/quote from array
 var randomIndex = Math.floor(Math.random() * quotes.length); 
 var randomString = quotes[randomIndex];
 // select word from selected string
-
-// shuffle word
-// display string
-//
+var newStr = randomString.split(" ");
+for (i in newStr ) {
+	var word = newStr[i];
+	// shuffle word
+	var newWord = _.shuffle(word).join("");
+	// join newWord back to string
+	newStr[i] = newWord;
+}
+var newQuote = newStr.join(" ");
+document.getElementById("quote").innerHTML = ' /" + newQuote + /" ';
 
 });
+
+	
+	
+
+
+
+
+
